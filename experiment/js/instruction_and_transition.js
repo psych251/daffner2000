d4658c3051ef209cfd956c1f59502ebcf2e61299
anjie cao <anjiecao@stanford.edu>
2020-10-21 06:50:49 -0700

    pre-pilotB

--- experiment/js/instruction_and_transition.js
+++ experiment/js/instruction_and_transition.js
@@ -1,3 +1,14 @@
+
+var front_page = {
+     type: "instructions",
+     pages:[
+         "By answering the following questions, you are participating in a study being performed by cognitive scientists in the Stanford Department of Psychology. If you have questions about this research, please contact us at <a href = 'mailto:stanfordpsych251@gmail.com'>stanfordpsych251@gmail.com</a>. You must be at least 18 years old to participate. Your participation in this research is voluntary. You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences. Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you. We have recently been made aware that your public Amazon.com profile can be accessed via your worker ID if you do not choose to opt out. If you would like to opt out of this feature, you may follow instructions available <a href='https://www.amazon.com/gp/help/customer/display.html?nodeId=16465241'>here</a>"
+     ],
+     show_clickable_nav: true
+     
+ }
+
+
 var main_instruction = {
      type: "instructions",
      pages:[
@@ -8,7 +19,9 @@ var main_instruction = {
          '<img src="images/instructions/prepractice_5.png" style="width:800px;height:600px;">',
          '<img src="images/instructions/prepractice_6.png" style="width:800px;height:600px;">',
          '<img src="images/instructions/prepractice_7.png" style="width:800px;height:600px;">',
-         '<img src="images/instructions/prepractice_8.png" style="width:800px;height:600px;">'
+         '<img src="images/instructions/prepractice_8.png" style="width:800px;height:600px;">',
+        '<img src="images/instructions/prepractice_9.png" style="width:800px;height:600px;">'
+
      ],
      show_clickable_nav: true
      
@@ -28,7 +41,7 @@ var final_instructions = {
    var final_thank_you = {
      type: "instructions",
      pages:[
-         "You are all done, thank you!"
+         "<p>You are all done, thank you!</p>In this study, we are interested in how complexity influences people's viewing time at different pictures. Past research has found that people spend longer time looking at pictures that are more complex. Please do not share this information with other potential participants."
      ],
      show_clickable_nav: false
      
