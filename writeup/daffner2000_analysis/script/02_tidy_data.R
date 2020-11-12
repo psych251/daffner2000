adfd05185441f645a58fcc8dbb003c3dee73deb4
anjie cao <anjiecao@stanford.edu>
2020-11-12 07:45:54 -0800

    updated model

--- writeup/daffner2000_analysis/script/02_tidy_data.R
+++ writeup/daffner2000_analysis/script/02_tidy_data.R
@@ -16,7 +16,14 @@ tidy_data <- function(filtered_data, output_path){
       demog_age = age, 
       demog_ethnicity = ethnicity, 
       demog_gender = gender, 
-      demog_education = education, 
+      demog_education = case_when(
+        education == "Some high school" ~ 1,
+        education == "High school diploma" ~ 2, 
+        education == "Associate Degree/Technical certification" ~ 3, 
+        education == "Bachelor's Degree" ~ 4, 
+        education == "Master's Degree" ~ 5, 
+        education == "Doctorate/Professional degree" ~ 6, 
+        education == "Other" ~ NA_real_),
       target_no_press_percent = no_press_prop,
       non_target_press_percent = wrong_press_prop,
       trial_pressed_space_bar = if_else(is.na(trial_pressed_space_bar), "no", trial_pressed_space_bar)
